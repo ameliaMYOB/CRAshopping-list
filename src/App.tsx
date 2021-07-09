5ee83158d93e10339f0495773ab5dcbc6e989b83
Braden Alsford <braden.alsford@myob.com>
2021-07-09 14:33:37 +1000

    pulls data out into another file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import './App.css';
 import React from "react";
 import ShoppingList from './Components/ShoppingList/ShoppingList'
-import { Item } from './types';
+import data from './data';
 
 function App() {
-    const bread: Item = {
-        name: "Bread",
-        category: "Bakery",
-        image: "https://thumbs.dreamstime.com/b/long-loaf-bread-22826883.jpg"
-    };
-    const milk: Item = {
-        name: "Milk",
-        category: "Dairy",
-        image: "https://igashop.com.au/wp-content/uploads/2020/05/A2-milk-full-cream.jpg"
-    };
-    const eggs: Item = {
-        name: "Eggs",
-        category: "Fresh",
-        image: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/why-are-eggs-good-for-you-1296x728-feature.jpg?w=1155&h=1528"
-    };
-
-    const shoppingList = [bread, milk, eggs];
+    const shoppingList = data;
 
     return (
       <div className="App">
