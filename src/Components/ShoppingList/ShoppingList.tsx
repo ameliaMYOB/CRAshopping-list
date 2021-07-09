3bc2714d9a7d8ad2f96f91bdab35243fe0145a14
Braden Alsford <braden.alsford@myob.com>
2021-07-09 15:04:29 +1000

    adds search functionality to the shopping list

--- src/Components/ShoppingList/ShoppingList.tsx
+++ src/Components/ShoppingList/ShoppingList.tsx
@@ -1,13 +1,27 @@
 import * as React from "react";
 import {Item} from "../../types";
 import ShoppingListItem from "./ShoppingList.Item";
+import './ShoppingList.css';
 
 const ShoppingList = (props: {items: Item[]}) => {
+
+    const [search, setSearch] = React.useState('');
+
     if (props.items.length > 0) {
-        const items = props.items.map(item => <ShoppingListItem item={item}/>);
+        const filteredItems = props.items.filter(item => item.name.toLowerCase().includes(search));
+
         return (
             <div>
-                {items}
+                <header className="header">
+                    <h1 className="title">Your amazing shopping list!</h1>
+                    <div className="searchBox">
+                        <label>Search: </label>
+                        <input value={search} onChange={event => setSearch(event.target.value)}/>
+                    </div>
+                </header>
+                <body>
+                    {filteredItems.map(item => <ShoppingListItem item={item}/>)}
+                </body>
             </div>
         )
     } else {
